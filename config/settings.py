54d6d04ac85a67abc057b59055a2a9253d0c3711
ffsasa <ngocan2003krp@gmail.com>
2024-10-15 17:19:39 +0700

    Update

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 GOOGLE_DRIVE_API_CREDENTIALS = os.path.join(os.path.dirname(__file__), 'credentials.json')  # Đường dẫn tới tệp JSON của API credentials
 GOOGLE_DRIVE_FOLDER_ID = '1ndpGD4nG6cAmUPZreEy36TpV2NJ4Q8e4'  # ID của thư mục trên Google Drive
 FIREBASE_CREDENTIALS = os.path.join(os.path.dirname(__file__), 'mindmath-firebase.json')  # Đường dẫn tới tệp JSON của API credentials
-STORAGE_BUCKET_ID = 'mindmath-a5aa6.appspot.com' # Storage bucket trên firebase
+STORAGE_BUCKET_ID = 'mindmath-56a26.appspot.com' # Storage bucket trên firebase
 
 REDIS_POST = 19901
 REDIS_HOST = 'redis-19901.c56.east-us.azure.redns.redis-cloud.com'
